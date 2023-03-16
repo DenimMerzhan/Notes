9cfe8aed4fac46c088654ec01286cec5198c84d9
Denim <dagn456@mail.ru>
2023-03-16 17:06:48 +0400

    Глава 2 добавлен 3-й ViewController

--- Notes/Controller/NotebookViewController.swift
+++ Notes/Controller/NotebookViewController.swift
@@ -6,21 +6,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class NotebookViewController: UIViewController {
 
-    var notesArray = ["Заметки","Песни","Блог"]
-    override func viewWillAppear(_ animated: Bool) {
-        let navigationBar = self.navigationController?.navigationBar
-        navigationBar?.barStyle = .black
-        navigationBar?.backgroundColor = UIColor(named: "NotesBackgound")
-    }
+    var category = String()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    @IBOutlet weak var userText: UITextView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(category)
+        loadData()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        let notesItem = NotesText(context: context)
+        notesItem.text = userText.text!
+        notesItem.category = category
+        print(category)
+        saveData()
     }
 
-
 }
+
+
+extension NotebookViewController {
+    func saveData(){
+        do{
+            try context.save()
+        }catch{
+            print("Ошибка сохранения данных - \(error)")
+        }
+    }
+    
+    func loadData() {
+        
+
+        let request = NSFetchRequest<NotesText>(entityName: "NotesText") /// мы приравниваем request ко всем элементам Notes в сущности Notes
+        let predicate = NSPredicate(format: "category == %@", category)
+        request.predicate = predicate
+        do{
+            let arr =  try context.fetch(request)
+            
+            if arr.count > 1 {
+                userText.text = arr[arr.count - 1].text
+                context.delete(arr[0])
+                saveData()
+                print(arr.count)
+            }else if arr.count != 0 {
+                userText.text = arr[arr.count - 1].text
+            }
+            
+        }catch{
+            print("Ошибка загрузки данных - \(error)")
+        }
+    }
+}
+
+
+
+
 
--- Notes/Controller/NotesViewController.swift
+++ Notes/Controller/NotesViewController.swift
@@ -6,31 +6,113 @@
 //
 
 import UIKit
+import CoreData
 
-class TableViewController: UITableViewController {
-
+class NotesViewController: UITableViewController {
+    
+    var category = ""
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var notesArray = [NotesCategory]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let navigationBar = self.navigationController?.navigationBar
+        navigationBar?.barStyle = .black
+        navigationBar?.backgroundColor = UIColor(named: "NotesBackgound")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadData()
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Добавить заметку", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { UITextField in
+            textField = UITextField
+        }
+        
+        let action = UIAlertAction(title: "Добавить", style: .default) { UIAlertAction in
+            print("Hey")
+            
+            if textField.text != ""{
+                let notesItem = NotesCategory(context: self.context)
+                print(textField.text ?? "nil")
+                notesItem.title = textField.text!
+                print("Wow")
+                self.notesArray.append(notesItem)
+                self.saveData()
+            }
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
     }
+    
+}
+    
+    // MARK: - Работа с ячейками
+    
+    
+    extension NotesViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return notesArray.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryNotesCell", for: indexPath)
+        cell.textLabel?.text = notesArray[indexPath.row].title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        category = notesArray[indexPath.row].title!
+        performSegue(withIdentifier: "goToNotes", sender: self)
 
-
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let destantionVC = segue.destination as! SecondNotesViewController
+            destantionVC.category = category
+            
+        }
 
 }
+
+
+// MARK: - Сохранение и чтение данных
+
+
+extension NotesViewController {
+    
+    func saveData(){
+        do {
+            try context.save()
+        }catch{
+            print("Ошибка сохранения данных - \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadData() {
+        let request = NSFetchRequest<NotesCategory>(entityName: "NotesCategory")
+        do {
+           notesArray =  try context.fetch(request)
+        }catch{
+            print("Ошибка чтения данных - \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
+
+
+
